b0ace98d641d0fc110722a6e4622b2980b54fc20
Santoh, Santosh <santosh-mnrec>
2021-01-03 18:43:33 +0800

    Added swagger

--- netcore/Startup.cs
+++ netcore/Startup.cs
@@ -90,7 +90,7 @@ namespace netcore
 
             // Get Super Admin Default options
             services.Configure<SuperAdminDefaultOptions>(Configuration.GetSection("SuperAdminDefaultOptions"));
-
+            services.AddSwaggerGen();
             services.AddMvc();
         }
 
@@ -107,7 +107,15 @@ namespace netcore
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthentication();
