ac991c295cd18a19aedffd8f4211f7ca40f04a15
Santoh, Santosh <santosh-mnrec>
2021-01-04 15:25:36 +0800

    Languange change

--- netcore/Models/Invent/Shipment.cs
+++ netcore/Models/Invent/Shipment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,6 +20,7 @@ namespace netcore.Models.Invent
 
         [StringLength(38)]
         [Display(Name = "Shipment Id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string shipmentId { get; set; }
 
         [StringLength(38)]
